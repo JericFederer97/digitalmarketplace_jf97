b5fa689f8bf5d04020167fe5da2590c8142a1880
j.enricobandong <j.enricobandong@gmail.com>
2020-04-13 22:04:23 +0900

    initial commit

---
+++ books/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+
+from .views import (
+    book_list,
+    book_detail,
+    chapter_detail,
+    exercise_detail,
+)
+
+
+app_name = "books"
+
+urlpatterns = [
+    path('', book_list, name="book_list"),
+    path('<slug>/', book_detail, name="book_detail"),
+    path('<book_slug>/<chapter_number>',
+         chapter_detail, name="chapter_detail"),
+    path('<book_slug>/<chapter_number>/<exercise_number>/',
+         exercise_detail, name="exercise_detail"),
+]
---
+++ books/views.py
@@ -0,0 +1,47 @@
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
+
+from .models import Book, Chapter, Exercise
+
+
+def book_list(request):
+    # Display a list of the books
+    queryset = Book.objects.all()
+    context = {
+        'queryset': queryset
+    }
+    return render(request, "book_list.html", context)
+
+
+def book_detail(request, slug):
+    # Display the book title and its chapters
+    book = get_object_or_404(Book, slug=slug)
+    context = {
+        'book': book,
+    }
+    return render(request, "book_detail.html", context)
+
+
+def chapter_detail(request, book_slug, chapter_number):
+    chapter_qs = Chapter.objects \
+        .filter(book__slug=book_slug) \
+        .filter(chapter_number=chapter_number)
+    if chapter_qs.exists():
+        context = {
+            "chapter": chapter_qs[0]
+        }
+        return render(request, "chapter_detail.html", context)
+    return Http404
+
+
+def exercise_detail(request, book_slug, chapter_number, exercise_number):
+    exercise_qs = Exercise.objects \
+        .filter(chapter__book__slug=book_slug) \
+        .filter(chapter__chapter_number=chapter_number) \
+        .filter(exercise_number=exercise_number)
+    if exercise_qs.exists():
+        context = {
+            "exercise": exercise_qs[0]
+        }
+        return render(request, "exercise_detail.html", context)
+    return Http404
