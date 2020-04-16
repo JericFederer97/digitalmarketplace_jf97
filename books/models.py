3312f679ca585b8725b880261d136004c277fbcd
j.enricobandong <j.enricobandong@gmail.com>
2020-04-16 15:22:18 +0900

    book app and templates added and styled

--- books/models.py
+++ books/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.shortcuts import redirect, reverse
 
 
 class Author(models.Model):
@@ -22,6 +23,11 @@ class Book(models.Model):
     def __str__(self):
         return self.title
 
+    def get_absolute_url(self):
+        return reverse('books:book_detail', kwargs={
+            'slug': self.slug,
+        })
+
 
 class Chapter(models.Model):
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
@@ -30,6 +36,12 @@ class Chapter(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('books:chapter_detail', kwargs={
+            'book_slug': self.book.slug,
+            'chapter_number': self.chapter_number,
+        })
 
 
 class Exercise(models.Model):
@@ -41,9 +53,17 @@ class Exercise(models.Model):
     def __str__(self):
         return self.title
 
+    def get_absolute_url(self):
+        return reverse('books:exercise_detail', kwargs={
+            'book_slug': self.chapter.book.slug,
+            'chapter_number': self.chapter.chapter_number,
+            'exercise_number': self.exercise_number,
+        })
+
 
 class Solution(models.Model):
     exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
+    solution_number = models.IntegerField()
     image = models.ImageField()
 
     def __str__(self):
--- books/urls.py
+++ books/urls.py
@@ -13,8 +13,8 @@ app_name = "books"
 urlpatterns = [
     path('', book_list, name="book_list"),
     path('<slug>/', book_detail, name="book_detail"),
-    path('<book_slug>/<chapter_number>',
+    path('<book_slug>/<int:chapter_number>',
          chapter_detail, name="chapter_detail"),
-    path('<book_slug>/<chapter_number>/<exercise_number>/',
+    path('<book_slug>/<int:chapter_number>/<int:exercise_number>/',
          exercise_detail, name="exercise_detail"),
 ]
